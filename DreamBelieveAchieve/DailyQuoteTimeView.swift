d803c9fbeea89df434590cdff06a77dec76a9219
github username <bballwiz4@gmail.com>
2020-10-14 20:42:58 -0700

    set up the picker for daily uote time and button to set the time.

--- DreamBelieveAchieve/DailyQuoteTimeView.swift
+++ DreamBelieveAchieve/DailyQuoteTimeView.swift
@@ -9,9 +9,38 @@
 import SwiftUI
 
 struct DailyQuoteTimeView: View {
+    @State private var selectedTime = Date()
     var body: some View {
-        Text("Daily Quote Time")
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [.red, .pink, .yellow]), startPoint: .bottomTrailing, endPoint: .topLeading)
+            VStack {
+                Text("Pick a time to be sent a quote")
+                HStack {
+                    DatePicker(selection: $selectedTime, displayedComponents: .hourAndMinute) {
+                        Text("Pick a time to be sent a quote")
+                    }
+                .labelsHidden()
+                }
+                HStack {
+                    Button(action: {
+                        self.setTimeForQuoteNotification()
+                    }) {
+                        Text("Set")
+                    }
+                    .frame(width: 100, height: 50)
+                    .background(Color.yellow)
+                    .clipShape(Capsule())
+                    .shadow(color: .gray, radius: 5, x: 0, y: 0)
+                }
+            }
+        }
+        .edgesIgnoringSafeArea(.all)
     }
+    
+    func setTimeForQuoteNotification() {
+        print("Set Time Ran!")
+    }
+    
 }
 
 struct DailyQuoteTimeView_Previews: PreviewProvider {
